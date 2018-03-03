4169a4453d6f28c5987970b88400e8fc9262c9ac
Top Dog <benpaul374@gmail.com>
2018-03-03 14:47:16 +1100

    WIP

--- crowds/prelim_analysis.py
+++ crowds/prelim_analysis.py
@@ -1,17 +1,64 @@
 import pandas as pd
 import numpy as np
+
 pd.set_option("display.max_rows", 150)
 
 df = pd.read_csv('../data/NRL.csv')
 
 # Annual crowd sizes
 
-print(df.head())
+temp = df.loc[df['Period'] != 'Finals', :]
+temp1 = temp.groupby('Year')['Crowd'].agg(['count', 'sum']).reset_index()
 
-temp = df.groupby('Year')['Crowd'].agg(['count', 'sum'])
-print(temp)
+temp1['base_growth'] = 0.0
+temp1['yearly_growth'] = 0.0
+temp1['yearly_change'] = 0
 
-temp2 = df.groupby('Period')['Crowd'].agg(['count', 'mean'])
+base_year = temp1.loc[0, 'sum']
+
+i = 0
+while i < len(temp1):
+	
+
+	if i == 0:
+		i += 1
+		continue
+	
+	current = temp1.loc[i, 'sum']
+	last = temp1.loc[i-1, 'sum']
+	
+	temp1.loc[i, 'base_growth'] = round(((current - base_year) / base_year)*100,1)
+	temp1.loc[i, 'yearly_growth'] = round(((current - last) / last)*100,1)
+	temp1.loc[i, 'yearly_change'] = current - last
+
+	i += 1
+
+temp1.to_csv('data/yearly_crowds.csv', index = False)
+
+temp2 = df.loc[df['Round'] != 'Grand Final', :]
+temp2 = df.groupby('Period')['Crowd'].agg(['count', 'mean']).reset_index()
+temp2.to_csv('data/period_crowd.csv', index = False)
+
+temp3 = temp.groupby('Home')['Crowd'].mean().round(0).reset_index()
+temp3.to_csv('data/teams_average.csv', index = False)
+
+# Broncos analysis
+
+broncs = temp.loc[temp['Home'] == 'Brisbane', :].reset_index()
+
+temp4 = broncs.groupby('Day')['Crowd'].agg(['count', 'mean']).reset_index()
+temp4.to_csv('data/broncs_days.csv', index = False)
+
+broncs['fri_other'] = broncs['Day'].apply(lambda x: 'Friday' if x == 'Fri' else 'Other')
+
+temp5 = broncs.groupby('fri_other')['Crowd'].count().reset_index()
+temp5.to_csv('data/broncs_fri_other.csv', index = False)
+
+# Day analysis of crowd size
+
+temp6 = temp.loc[~temp['Day'].isin(['Tue', 'Wed']), :]
+temp6 = temp6.groupby('Day')['Crowd'].mean().round(0).reset_index()
+temp6.to_csv('data/days_crowds.csv', index = False)
 
 # Create column which is home if team is at a home group and neutral overwise
 
@@ -75,11 +122,9 @@ ladder_df = pd.read_csv('../data/nrl_ladder.csv')
 
 temp = pd.merge(crowds, ladder_df, how = 'inner', left_on = ['Year', 'Team'], right_on = ['Year', 'Team'])
 
-temp = temp[['Year', 'Team', 'test', 'Rank']]
+temp = temp[['Year', 'Team', 'mean', 'test', 'Rank']]
 
-print(crowds.head(32))
-print(ladder_df.head(32))
 print(temp)
 
 
-temp.to_csv('test.csv', index = False)
+temp.to_csv('data/test.csv', index = False)
--- crowds/r_visual.R
+++ crowds/r_visual.R
@@ -1,7 +1,99 @@
 library('ggplot2')
+library('scales')
 
-df <- read.csv('test.csv')
+df <- read.csv('data/yearly_crowds.csv')
 
-ggplot(df, aes(x = Rank, y = test, color = Team)) +
+ggplot(df, aes(x = Year, y = sum)) +
+	geom_bar(stat = 'identity', fill = 'steelblue') +
+	scale_y_continuous(labels = comma) +
+	scale_x_continuous(breaks = seq(2007, 2017, 1))
+ggsave(filename = 'graphs/yearly_crowds.pdf')
+
+ggplot(df, aes(x = Year, y = base_growth)) +
+	geom_line() +
 	geom_point() +
-	geom_smooth(method = 'lm', se = FALSE)
+	scale_x_continuous(breaks = seq(2007, 2017, 1))
+ggsave(filename = 'graphs/base_growth.pdf')
+
+ggplot(df, aes(x = Year, yearly_growth)) +
+	geom_line() +
+	geom_point() +
+	scale_x_continuous(breaks = seq(2007, 2017, 1))
+ggsave(filename = 'graphs/yearly_growth.pdf')
+
+ggplot(df, aes(x = Year, y = yearly_change)) +
+	geom_bar(stat = 'identity', fill = 'steelblue') +
+	scale_y_continuous(labels = comma) +
+	scale_x_continuous(breaks = seq(2007, 2017, 1))
+ggsave(filename = 'graphs/yearly_change.pdf')
+
+df <- read.csv('data/period_crowd.csv')
+
+ggplot(df, aes(x = Period, y = mean)) +
+	geom_bar(stat = "identity", fill = 'steelblue') +
+	labs(y = 'Average Attendance Per Game')
+ggsave(filename = 'graphs/period_change.pdf')
+
+df <- read.csv('data/test.csv')
+
+teams_list <- unique(df$Team)
+
+bandwagon <- function(team) {
+	temp <- df[df$Team == team, ]
+
+	ggplot(temp, aes(x = Rank, y = mean)) +
+		geom_point() +
+		geom_smooth(method = lm, se = FALSE) +
+		geom_text(label = temp$Year, hjust = 0)
+	ggsave(paste('graphs/teams/',team,'_bandwagon.pdf', sep = ""))
+}
+
+
+for (team in teams_list) {
+	bandwagon(team)
+}
+
+df <- read.csv('data/teams_average.csv')
+
+sorted_df <- df[order(-df$Crowd), ]
+positions <- sorted_df$Home
+
+ggplot(df, aes(x = Home, y = Crowd)) +
+	geom_bar(stat = "identity", fill = 'steelblue') +
+	scale_y_continuous(breaks = seq(0,30000,5000)) +
+	scale_x_discrete(limits = positions) +
+	theme(axis.text.x = element_text(angle = 90)) +
+	labs(title = 'Average Home Crowd 2008-2017', y = 'Average', x = 'Team')
+ggsave(filename = 'graphs/teams_average.pdf')
+
+########## Broncos ##########
+
+df <- read.csv('data/broncs_days.csv')
+
+ggplot(df, aes(x = Day, y = count)) +
+	geom_bar(stat = 'identity', fill = 'steelblue')
+ggsave(filename = 'graphs/broncos_days.pdf')
+
+
+df <- read.csv('data/broncs_fri_other.csv')
+
+ggplot(df, aes(x = fri_other, y = Crowd)) +
+	geom_bar(stat = 'identity', fill = 'steelblue')
+ggsave(filename = 'graphs/broncos_fri_other.pdf')
+
+
+########## Day of week ##########
+
+df <- read.csv('data/days_crowds.csv')
+
+positions <- c('Thu', 'Fri', 'Sat', 'Sun', 'Mon')
+
+ggplot(df, aes(x = Day, y = Crowd)) +
+	geom_bar(stat = "identity", fill = "steelblue") +
+	scale_x_discrete(limits = positions) +
+	labs(title = 'Average Crowd By Day', y = 'Average Crowd', x = 'Day')
+ggsave(filename = 'graphs/days.pdf')
+
+
+
+
